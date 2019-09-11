aa777c05f6d7f058a4238b8ae0c479107cd8b1e6
kiwilss <m15205622327@163.com>
2019-09-11 17:39:04 +0800

    ktx add

---
+++ xktx/src/main/java/com/kiwilss/lxkj/xktx/anko/Intents.kt
@@ -0,0 +1,103 @@
+/**
+ * Copyright (C), 2017-2019, XXX有限公司
+ * FileName: Intents
+ * Author:   kiwilss
+ * Date:     2019-09-11 17:27
+ * Description: {DESCRIPTION}
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+package com.kiwilss.lxkj.xktx.anko
+
+
+import android.annotation.SuppressLint
+import android.content.ActivityNotFoundException
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+
+/**
+ *@FileName: Intents
+ *@author : Lss kiwilss
+ * @e-mail : kiwilss@163.com
+ * @time   : 2019-09-11
+ * @desc   : {DESCRIPTION}
+ */
+
+fun Context.browse(url: String, newTask: Boolean = false): Boolean {
+    try {
+        val intent = Intent(Intent.ACTION_VIEW)
+        intent.data = Uri.parse(url)
+        if (newTask) {
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        }
+        startActivity(intent)
+        return true
+    } catch (e: ActivityNotFoundException) {
+        e.printStackTrace()
+        return false
+    }
+}
+
+
+fun Context.share(text: String, subject: String = ""): Boolean {
+    try {
+        val intent = Intent(Intent.ACTION_SEND)
+        intent.type = "text/plain"
+        intent.putExtra(android.content.Intent.EXTRA_SUBJECT, subject)
+        intent.putExtra(android.content.Intent.EXTRA_TEXT, text)
+        startActivity(Intent.createChooser(intent, null))
+        return true
+    } catch (e: ActivityNotFoundException) {
+        e.printStackTrace()
+        return false
+    }
+}
+
+
+fun Context.email(email: String, subject: String = "", text: String = ""): Boolean {
+    val intent = Intent(Intent.ACTION_SENDTO)
+    intent.data = Uri.parse("mailto:")
+    intent.putExtra(Intent.EXTRA_EMAIL, arrayOf(email))
+    if (subject.isNotEmpty())
+        intent.putExtra(Intent.EXTRA_SUBJECT, subject)
+    if (text.isNotEmpty())
+        intent.putExtra(Intent.EXTRA_TEXT, text)
+    if (intent.resolveActivity(packageManager) != null) {
+        startActivity(intent)
+        return true
+    }
+    return false
+
+}
+
+
+
+@SuppressLint("MissingPermission")
+fun Context.makeCall(number: String): Boolean {
+    try {
+        val intent = Intent(Intent.ACTION_CALL, Uri.parse("tel:$number"))
+        startActivity(intent)
+        return true
+    } catch (e: Exception) {
+        e.printStackTrace()
+        return false
+    }
+}
+
+
+fun Context.sendSMS(number: String, text: String = ""): Boolean {
+    try {
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse("sms:$number"))
+        intent.putExtra("sms_body", text)
+        startActivity(intent)
+        return true
+    } catch (e: Exception) {
+        e.printStackTrace()
+        return false
+    }
+}
+
+
+
